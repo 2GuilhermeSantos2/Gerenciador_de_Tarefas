04757818a7f8ecf17681c388481834fbee6ae3c0
Guilherme Santos <80928105+2GuilhermeSantos2@users.noreply.github.com>
2024-01-25 20:45:14 -0300

    Hoje eu terminei, de fazer as opções do menu hambúguer, adicionando os icones e texto. Replicando tudo na mão em ctrl c ctrl v.

---
+++ app/src/main/java/com/example/gerenciartarefas/Notas.kt
@@ -0,0 +1,30 @@
+package com.example.gerenciartarefas
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.gerenciartarefas.databinding.ActivityNotasBinding
+
+class Notas : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column {
+                Text(text =
+                "Hello"
+                )
+            }
+        }
+    }
+}
+
+
